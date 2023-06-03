78b958dd0180f4c81dcf1591a2f4700574b79e37
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-06-02 22:27:11 -0300

    first commit

---
+++ workerProject/workerProject/Entities/Worker.cs
@@ -0,0 +1,22 @@
+﻿using workerProject.Entities.Enums;
+
+namespace workerProject.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+    }
+}
